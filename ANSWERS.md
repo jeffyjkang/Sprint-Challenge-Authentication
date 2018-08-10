cde5b3c242cc1971388dd4107e4c882f4b043f3f
jeffyjkang <jeffyjkang@gmail.com>
2018-08-10 09:12:28 -0700

    jeff kang auth sprint challenge

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,6 +2,14 @@
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
 
+    sessions are used to store information about a client so there is no need to constantly re-enter credentials everytime there is a new request to the server express sessions utilize cookies to do this.
+    bcrypt is a hashing function, it allows an algorithm to hash information multiple times called rounds.
+    jwts or json web tokens are cryptographically signed middleware that default to the sha-256 algorithm, difference between jwts and cookies would be state.
+
 2.  What does bcrypt do in order to prevent attacks?
 
+    brypt utilizes hashing which hinders attackers from vital information. the hashing process goes through multiple rounds which requires the attacker to undo the hash, know which algorithm the hash used and how many rounds the hash underwent in order to generate the hash.
+
 3.  What are the three parts of the JSON Web Token?
+
+    three parts of json web token are the header, the payload and the signature, the header contains the algorithm of the token type, the payload contains the claims, or info about what we would like the token to store, ie the id. the signature is like the secret of the string that contains the three parts including the header, payload and secret and creates a string.
--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,67 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
+
+const db = require("../database/dbConfig");
+const bcrypt = require("bcryptjs");
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+  db("users")
+    .insert(user)
+    .then(ids => {
+      db("users")
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          const token = generate(user);
+          res.status(201).json(token);
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const credentials = req.body;
+  db("users")
+    .where({
+      username: credentials.username
+    })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generate(user);
+        res.send(token);
+      } else {
+        return res.status(401).json({ error: "Incorrect credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "error" });
+    });
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
